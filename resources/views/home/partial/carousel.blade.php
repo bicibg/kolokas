8f10c96a772bd9c0f381c6e38a5209299cacb960
Bugra Ergin <bugra.ergin@hotelcard.com>
2020-06-24 21:44:10 +0200

    fixed carousel not carouseling, fixed search box styling

--- resources/views/home/partial/carousel.blade.php
+++ resources/views/home/partial/carousel.blade.php
@@ -1,4 +1,4 @@
-<div id="myCarousel" class="carousel slide">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         @php
             $active = true
