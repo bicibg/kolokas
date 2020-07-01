35310f233591557c531d905083bf6ef156b76154
Bugra Ergin <bugra.ergin@hotelcard.com>
2020-07-01 13:33:15 +0200

    fixed home page queries

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,19 +22,18 @@ class HomeController extends Controller
         $carousel = Recipe::wherePublished(true)->whereFeatured(true)->get();
         $carousel = $carousel->random(min(5, $carousel->count()));
 
-        $mostFavourited = Recipe::wherePublished(true)->get();
-        $mostFavourited = $mostFavourited->sortByDesc('favouritesCount')->take(4)->values()->all();
+        $mostFavourited = Recipe::withCount('favourites')->wherePublished(true);
+        $mostFavourited = $mostFavourited->orderByDesc('favourites_count')->limit(4)->get();
 
-        $mostVisited = Recipe::wherePublished(true)->get();
-        $mostVisited = $mostVisited->sortByDesc('visitedCount')->take(4)->values()->all();
-
-        $latest = Recipe::wherePublished(true)->latest()->limit(4)->get();
+        $mostVisited = Recipe::withCount('visits')->wherePublished(true)->orderByDesc('visits_count')->limit(4)->get();
 
         $contributors = Profile::all();
         $contributors = $contributors->random(min(4, $contributors->count()));
+        $latest = Recipe::wherePublished(true)->latest()->limit(4)->get();
 
         $categories = Category::all();
 
-        return view('home.index', compact('latest', 'featured', 'carousel', 'mostFavourited', 'mostVisited', 'contributors', 'categories'));
+        return view('home.index',
+            compact('latest', 'featured', 'carousel', 'mostFavourited', 'mostVisited', 'contributors', 'categories'));
     }
 }
