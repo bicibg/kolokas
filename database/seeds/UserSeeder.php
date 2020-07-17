de42724c77dbdfcd05dd584006a66fdc81606a09
Bugra Ergin <bugra.ergin@hotelcard.com>
2020-07-17 09:59:32 +0200

    added Burak to user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,15 +13,35 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = factory(User::class)->create([
-            'email' => 'bugraergin@gmail.com',
-            'password' => bcrypt('123456789'),
-        ]);
-        factory(Profile::class)->create([
-            'email' => $user->email,
-            'name' => 'Bugra Ergin',
-            'user_id' => $user->id,
-        ]);
+        $admins = [
+            [
+                'user' => [
+                    'email' => 'bugraergin@gmail.com',
+                    'password' => bcrypt('123456789'),
+                ],
+                'profile' => [
+                    'name' => 'Bugra Ergin',
+                    'email' => 'bugraergin@gmail.com',
+                ]
+            ],
+            [
+                'user' => [
+                    'email' => 'burakergin95@gmail.com',
+                    'password' => '$2y$10$AkMeLgCxf/3iXVkOXSJrc.MItFMQuetMSAan1IiWxWjx.6131Rm3.',
+                ],
+                'profile' => [
+                    'name' => 'Bugra Ergin',
+                    'email' => 'bugraergin@gmail.com',
+                ]
+            ]
+        ];
+
+        foreach ($admins as $admin) {
+            $user = factory(User::class)->create($admin['user']);
+            $admin['profile']['user_id'] = $user->id;
+            factory(Profile::class)->create($admin['profile']);
+        }
+
         factory(Profile::class, 30)->create();
     }
 }
