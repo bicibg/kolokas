54c21c885ac050fa48856475802c3dab36ad1b8e
Bugra Ergin <bugra.ergin@hotelcard.com>
2021-02-16 15:21:42 +0100

    fix: changed contact email on demo page

--- resources/lang/en/demo.php
+++ resources/lang/en/demo.php
@@ -2,7 +2,7 @@
 
 return [
     'no_access' => 'Kolokas.com is currently accessible by invite only for testing purposes. If you don\'t have an access key, check back later when we are live.',
-    'contact' => 'If you don\'t have an access key but would like to help with testing or translating Kolokas.com, you can contact me on <a href=\'mailto:bugraergin@gmail.com\' target=\'_blank\'>bugraergin@gmail.com</a>',
+    'contact' => 'If you don\'t have an access key but would like to help with testing or translating Kolokas.com, you can contact me on <a href=\'mailto:kolokas.recipes@gmail.com\' target=\'_blank\'>kolokas.recipes@gmail.com</a>',
     'letmein' => 'Let me in',
     'access_key' => 'Access key',
 ];
--- resources/lang/tr/demo.php
+++ resources/lang/tr/demo.php
@@ -2,7 +2,7 @@
 
 return [
     'no_access' => 'Kolokas.com\'a şu anda test edebilmek amacıyla yalnızca davetle erişilebilir. Sitemiz acıldıktan sonra sizleri tekrar görmek isteriz.',
-    'contact' => 'Bir erişim anahtarınız yoksa ve Kolokas.com\'u test etmek veya çevirilerde yardım etmek istiyorsanız, <a href=\'mailto:bugraergin@gmail.com\' target=\'_blank\'>bugraergin@gmail.com</a> adresinden bizimle iletişime geçebilirsiniz.',
+    'contact' => 'Bir erişim anahtarınız yoksa ve Kolokas.com\'u test etmek veya çevirilerde yardım etmek istiyorsanız, <a href=\'mailto:kolokas.recipes@gmail.com\' target=\'_blank\'>kolokas.recipes@gmail.com</a> adresinden bizimle iletişime geçebilirsiniz.',
     'letmein' => 'İzin ver',
     'access_key' => 'Erişim anahtarı',
 ];
